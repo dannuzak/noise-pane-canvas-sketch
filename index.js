4b2e19a85464c7a8025e2dfa469232c298f2db18
Daniela Zacarias <daniela.zacarias@outlook.com>
2023-11-09 15:52:03 +0000

    installed esm to solve error with import of Tweakpane

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 const math = require('canvas-sketch-util/math')
+const Tweakpane = require('tweakpane')
 
 const settings = {
-    dimensions: [1080, 1080]
+    dimensions: [1080, 1080],
+    animate: true
+};
+
+const params = {
+    cols: 10,
+    rows: 10,
 }
 
 const sketch = () => {
-    return ({ context, width, height }) => {
+    return ({ context, width, height, frame }) => {
         context.fillStyle = 'white';
         context.fillRect(0, 0, width, height);
-        const cols = 10;
-        const rows = 10;
+
+        const cols = params.cols;
+        const rows = params.rows;
         const numCells = cols * rows;
+
         const gridw = width * 0.8;
         const gridh = height * 0.8;
         const cellw = gridw / cols;
@@ -29,13 +38,10 @@ const sketch = () => {
             const w = cellw * 0.8;
             const h = cellh * 0.8;
 
-            const n = random.noise2D(x, y, 0.001); // noise2d gives us anumber between -1 and 1 and when multiplied by Math.PI we get the equivalent of - 180 degrees to 180 degrees and to see it we need to rotate our context by angle
-            //Right now the frequency is too high, because of the big dif between x and y, we fix adding the 3rd param in noise which is frequency. We also add amplitud, not as 4th param of noise, but to angle and the lines start flowing nicely.
+            const n = random.noise2D(x + frame * 10, y, 0.001);
 
             const angle = n * Math.PI * 0.2;
 
-            //const scale = (n + 1) / 2 * 30;
-            //const scale = (n * 0.5 + 0.5) * 30;
             const scale = math.mapRange(n, -1, 1, 1, 30);
 
             context.save();
@@ -53,6 +59,18 @@ const sketch = () => {
 
             context.restore();
         }
+
     };
 };
+
+const createPane = () => {
+    const pane = new Tweakpane.Pane();
+    let folder;
+
+    folder = pane.addFolder({ title: 'Grid' });
+    folder.addInput(params, 'cols', { min: 2, max: 50, step: 1 });
+    folder.addInput(params, 'rows', { min: 2, max: 50, step: 1 });
+}
+
+createPane();
 canvasSketch(sketch, settings);
